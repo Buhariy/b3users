07f1418aaad4c67819b52c391f4fcbc8803d33a6
Buhariy <buhari.yavuz@gmail.com>
2019-09-25 18:42:14 +0200

    CRUD FINIS manque bootstrap

--- Liste_user.php
+++ Liste_user.php
@@ -14,35 +14,29 @@ $sql = "SELECT * FROM user";
 
 $stmt = $link->prepare($sql);
 $stmt->execute();
-
-/*$result = $stmt->fetchAll();
-print_r($result);*/
-
 ?>
 
 <html>
-	<body>
-			<h1>Liste user :</h1>
-	<table border="1" class="toto"><style type="text/css">.toto {border-color:blue;}</style>
-	<tr>
-		<th>
-		nom
-		</th>
-		<th>
-		prenom
-		</th>
-		<th>
-		num
-		</th>
-		<!--<th>
-		supprimer
-		</th>
-		<th>
-		modifier
-		</th>-->
-	</tr>
-	<?php foreach($stmt as $user){ ?>
-	<tr>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+	<h1>Liste des utilisateurs :</h1>
+	<center><table border="1" class="toto"><style type="text/css">.toto {border-color:blue;}</style>
+		<tr>
+			<th>
+			Nom
+			</th>
+			<th>
+			Prenom
+			</th>
+			<th>
+			Num
+			</th>
+		</tr>
+		<?php foreach($stmt as $user){ ?>
+		<tr>
 		<td>
 			<?php echo $user['nom']."<br>"; ?> 
 		</td>
@@ -52,20 +46,17 @@ print_r($result);*/
 		<td>
 			<?php echo $user['num']."<br>"; ?>
 		</td>
-		<!--<td>
-		<input type="submit" name="sendDL" value="Supprimer"/>
-		</td>
-		<td>
-		<input type="submit" name="sendUP" value="Update"/>
-		</td>-->
-	</tr>
-	<?php } ?>
+		</tr>
+		<?php } ?>
 	</table>
+	</center>
+	<br/>
+	<center>
 	<table>
 		<form method="post" action="" >
 		<tr>
 			<td>
-				<input type="submit" name="sendDL" value="Supprimer"/>
+				<input type="submit" name="sendDL" class="btn btn-primary" value="Supprimer"/>
 			</td>
 			<td>
 				<input type="text" name="DLsuppr" />
@@ -73,49 +64,57 @@ print_r($result);*/
 		</tr>
 		</form>
 	</table>
+	<br/>
+	</center>
 	<form method="post" action="" >
-	<table border="1">
-	<th>
-	MaJ
-	</th>
-	<th>
-	<input type="submit" name="sendUP" value="Update"/>
-	</th>	
-	<tr>
-		<td>
-			Nom :
-		</td>
-		<td>
-			<input type="text" name="majareaN" />
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Prenom :
-		</td>
-		<td>
-			<input type="text" name="majareaP" />
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Num :
-		</td>
-		<td>
-			<input type="text" name="majareaNum" />
-		</td>
-	</tr>
-	<tr>
-		<td>
-			sélectionner nom a modifier :
-		</td>
-		<td>
-			<input type="text" name="select" />
-		</td>
-	</tr>
+	<center>
+		<table border="1">
+		<th>
+		MaJ
+		</th>
+		<th>
+		<input type="submit" name="sendUP" value="Update" class="btn btn-primary" id="sendUPDATE"/>
+		</th>	
+		<tr><!--La maj d'un fichier-->
+			<td>
+				Nom :
+			</td>
+			<td>
+				<input type="text" name="majareaN" />
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Prenom :
+			</td>
+			<td>
+				<input type="text" name="majareaP" />
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Num :
+			</td>
+			<td>
+				<input type="text" name="majareaNum" />
+			</td>
+		</tr>
+		<tr>
+			<td>
+				sélectionner nom a modifier :
+			</td>
+			<td>
+				<input type="text" name="select" />
+			</td>
+		</tr>
 	</table>
+	</center>
 	</form>
-	</body>
+	<a href="http://localhost/exophp/users/formulaire.php">link</a>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
 <?php
 
@@ -128,26 +127,13 @@ $stmt = $link->prepare($sql);
 $stmt->execute(array(
 	"prenom" => $_POST['DLsuppr'],
 ));
-header('Location: http://localhost/exophp/Liste_user.php');
+header('Location: http://localhost/exophp/users/Liste_user.php');
 }
 
-if(!empty($_POST['majarea'])){
+if(!empty($_POST['sendUP'])){
 $link=connect();
-/*
-$sql = "UPDATE `user` SET
- `nom`="$_POST['majareaN']",
- `prenom`="$_POST['majareaP']"',
- `num`="$_POST['majareaNum']"
- WHERE
- `nom`="$_POST['select']";";
-*/
-$stmt = $link->prepare($sql);
-$stmt->execute(array(
-	"prenom" => $_POST['DLsuppr'],
-));
-header('Location: http://localhost/exophp/Liste_user.php');
+$stmt = $link->prepare("UPDATE user SET nom = ?, prenom = ?, num = ? WHERE nom = ? ");
+$stmt->execute(array($_POST['majareaN'],$_POST['majareaP'],$_POST['majareaNum'],$_POST['select']));
+header('Location: http://localhost/exophp/users/Liste_user.php');
 }
-
-
-//
 ?>
--- formulaire.php
+++ formulaire.php
@@ -1,26 +1,9 @@
-<?php 
-/*
-session_start();
-if (!isset($_SESSION['nom'])){
-$_SESSION['nom']=$_POST['nom'];}
-if (!isset($_SESSION['prenom'])){
-$_SESSION['prenom']=$_POST['prenom'];}	
-if (!isset($_SESSION['num'])){
-$_SESSION['num']=$_POST['num'];}
-
-if(check($_POST)){
-$tab[]= $_SESSION['nom'];
-$tab[]= $_SESSION['prenom'];
-$tab[]= $_SESSION['num'];
-
-echo "<pre>";
-print_r($tab);
-echo "</pre>";
-}*/
-?>
 <html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 	<body>
-		<h1>Liste user :</h1>
+		<h1>Liste user :</h1><br/>
 		<form method="post" action="" >
 			<p>Formulaire :</p>
 			Nom:<input type="text" name="nom" />
@@ -29,6 +12,9 @@ echo "</pre>";
 			<input type="submit" name="send" value="OK"/>
 		</form>
 	</body>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
 
 
@@ -63,7 +49,6 @@ $stmt->execute(array(
 	"prenom" => $_POST['prenom'],
 	"num" => $_POST['num'],
 ));
-header('Location: http://localhost/exophp/Liste_user.php');
+header('Location: http://localhost/exophp/users/Liste_user.php');
 }
 ?>
-UPDATE `user` SET `nom`='maj',`prenom`='maj',`num`='2727' WHERE `nom`='adam';
